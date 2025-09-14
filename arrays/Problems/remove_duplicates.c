c03650b46986d6bfde122221cd95479700339a2a
Aayush Raj <aayushr679@gmail.com>
2025-09-14 23:47:34 +0530

    Create remove_duplicates.c

---
+++ arrays/Problems/remove_duplicates.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int removeDuplicates(int A[], int n) {
+    if (n == 0) return 0;
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+        if (A[i] != A[j]) {
+            j++;
+            A[j] = A[i];
+        }
+    }
+    return j + 1;
+}
+
+void printArray(int A[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main() {
+    int A[] = {1, 1, 2, 2, 3, 3, 4, 5, 5};
+    int n = 9;
+    n = removeDuplicates(A, n);
+    printArray(A, n);
+    return 0;
+}

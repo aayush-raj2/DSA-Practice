d809b5661a2a6a56b53564f912d1bc2facb5923d
Aayush Raj <aayushr679@gmail.com>
2025-09-18 23:30:06 +0530

    Update reverse_array.c

--- arrays/Problems/reverse_array.c
+++ arrays/Problems/reverse_array.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
+// Function to reverse an array in-place
 void reverse(int A[], int n) {
     int start = 0, end = n - 1;
+
     while (start < end) {
         int temp = A[start];
         A[start] = A[end];
         A[end] = temp;
+
         start++;
         end--;
     }
 }
 
+// Function to print the array
 void printArray(int A[], int n) {
-    for (int i = 0; i < n; i++) printf("%d ", A[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
     printf("\n");
 }
 
+// Driver code
 int main() {
     int A[] = {1, 2, 3, 4, 5};
     int n = 5;
+
     reverse(A, n);
     printArray(A, n);
+
     return 0;
 }

dc1661ae2aaae0fd93e279f1e5c3ff13160f3148
Aayush Raj <aayushr679@gmail.com>
2025-10-01 23:21:02 +0530

    Create factorial_function.c

---
+++ Functions/factorial_function.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}

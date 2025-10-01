73a9004a24c9ac530cefa0ddb42d6056710977e9
Aayush Raj <aayushr679@gmail.com>
2025-10-01 23:20:14 +0530

    Create add_function.c

---
+++ Functions/add_function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Function to add two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 10, y = 5;
+    printf("Sum is %d\n", add(x, y));// calling function
+    return 0;
+}
